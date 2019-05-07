b5529847e0c4a2e245fb350045d43f7fb790851d
Gregory Ashton <gregory.ashton@ligo.org>
2019-05-08 08:55:07 +1000

    Initial attempt at providing live_points to dynesty

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -449,17 +449,17 @@ class Sampler(object):
 
         Returns
         -------
-        bool, cube (nlive,
+        bool,
             True if the likelihood and prior are finite, false otherwise
 
         """
-        if np.isinf(self.log_prior(theta)):
-            if warning:
-                logger.warning('Prior draw {} has inf prior'.format(theta))
-            return False
         if np.isinf(self.log_likelihood(theta)):
             if warning:
                 logger.warning('Prior draw {} has inf likelihood'.format(theta))
+            return False
+        if np.isinf(self.log_prior(theta)):
+            if warning:
+                logger.warning('Prior draw {} has inf prior'.format(theta))
             return False
         return True
 
