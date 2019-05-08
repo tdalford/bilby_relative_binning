fc019f538126a74277ee2c49cb9ac9c822ca8a10
Gregory Ashton <gregory.ashton@ligo.org>
2019-05-08 02:08:57 -0500

    Update bilby/core/sampler/base_sampler.py

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -420,11 +420,9 @@ class Sampler(object):
         Returns
         -------
         unit_cube, parameters, likelihood: tuple of array_like
-            unit_cube (nlive, ndim) is an array of the prior samples from the
-            unit cube, parameters (nlive, ndim) is the unit_cube array
-            transformed to the target space, while likelihood (nlive) are the
-            likelihood evaluations.
-
+            unit_cube (nlive, ndim) is an array of the prior samples from the unit cube,
+            parameters (nlive, ndim) is the unit_cube array transformed to the target space while
+            likelihood (nlive) are the likelihood evaluations. 
         """
         unit_cube = []
         parameters = []
@@ -449,7 +447,7 @@ class Sampler(object):
 
         Returns
         -------
-        bool,
+        bool, cube (nlive,
             True if the likelihood and prior are finite, false otherwise
 
         """
