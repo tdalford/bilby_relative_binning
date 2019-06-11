02738ad64141c3f75850c9708c5f4b24ba526f21
Colm Talbot <colm.talbot@ligo.org>
2019-06-11 12:29:15 +1000

    remove null jacobian

--- bilby/core/prior.py
+++ bilby/core/prior.py
@@ -13,7 +13,6 @@ from scipy.special import erf, erfinv
 
 # Keep import bilby statement, it is necessary for some eval() statements
 import bilby  # noqa
-from .jacobian import null_jacobian
 from .utils import (
     import_from_string, logger, infer_args_from_method,
     check_directory_exists_and_if_not_mkdir)
@@ -33,11 +32,14 @@ class PriorDict(OrderedDict):
         conversion_function: func
             Function to convert between sampled parameters and constraints.
             Default is no conversion.
-        jacobian: func
-            If the prior is not separable, this can add some degeneracies.
-            Care should be taken with the rescaling, as this will break down
-            with a non-trivial Jacobian.
-            The function should take a dictionary and return the Jacobian.
+        jacobian: func, optional
+            Jacobian for converting parameters defined in the prior to
+            parameters used during sampling. The function should take a
+            dictionary and return the determinant of the Jacobian matrix.
+            Note, if the prior is not separable, this can add degeneracy.
+            Rescaling of the prior will probably no longer make sense when
+            using non-trivial Jacobians.
+            Default is None will not apply a Jacobian
         """
         OrderedDict.__init__(self)
         if isinstance(dictionary, dict):
@@ -60,9 +62,7 @@ class PriorDict(OrderedDict):
         self._total_samples = 0
         self._accepted_samples = 0
 
-        if jacobian is None:
-            self.jacobian = null_jacobian
-        elif not hasattr(self, 'jacobian'):
+        if not hasattr(self, 'jacobian'):
             self.jacobian = jacobian
 
     def evaluate_constraints(self, sample):
