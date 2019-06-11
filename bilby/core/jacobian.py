6952e26d9a6df168036c3d778715929b0a84b380
Colm Talbot <colm.talbot@ligo.org>
2019-06-11 11:23:27 +1000

    make null jacobian safe for multiple samples

--- bilby/core/jacobian.py
+++ bilby/core/jacobian.py
@@ -1,3 +1,6 @@
-def null_jacobian(*args, **kwargs):
+import numpy as np
+
+
+def null_jacobian(sample):
     """Dummy jacobian function, returns 1"""
-    return 1
+    return np.ones_like(list(sample.values())[0])
