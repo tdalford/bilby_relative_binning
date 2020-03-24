eb132354a8201a99393ae36040c73b998fc791f9
Colm Talbot <colm.talbot@ligo.org>
2020-03-24 11:12:57 -0400

    Update logo script

--- examples/core_examples/logo/sample_logo.py
+++ examples/core_examples/logo/sample_logo.py
@@ -1,4 +1,13 @@
-""" Script used to generate the samples for the bilby logo """
+"""
+Script used to generate the samples for the bilby logo
+
+Extra requirements
+==================
+- scikit-image: package for reading images into an array, available via pypi
+- nestle: a pure-python nested sampling package, available via pypi
+
+Typical run time: ~ 1 mintue
+"""
 import bilby
 import numpy as np
 import scipy.interpolate as si
@@ -7,26 +16,30 @@ from skimage import io
 
 class Likelihood(bilby.Likelihood):
     def __init__(self, interp):
+        super().__init__(parameters=dict())
         self.interp = interp
-        super().__init__(parameters=dict(x=None, y=None))
 
     def log_likelihood(self):
-        return -1 / (self.interp(self.parameters['x'], self.parameters['y'])[0])
+        return -1 / (self.interp(self.parameters["x"], self.parameters["y"])[0])
 
 
-for letter in ['B', 'I', 'L', 'Y']:
-    img = 1 - io.imread('{}.png'.format(letter), as_gray=True)[::-1, :]
+for letter in ["B", "I", "L", "Y"]:
+    img = 1 - io.imread("{}.png".format(letter), as_gray=True)[::-1, :]
     x = np.arange(img.shape[0])
     y = np.arange(img.shape[1])
     interp = si.interpolate.interp2d(x, y, img.T)
 
     likelihood = Likelihood(interp)
 
-    priors = {}
-    priors['x'] = bilby.prior.Uniform(0, max(x), 'x')
-    priors['y'] = bilby.prior.Uniform(0, max(y), 'y')
+    priors = bilby.core.prior.PriorDict()
+    priors["x"] = bilby.prior.Uniform(0, max(x), "x")
+    priors["y"] = bilby.prior.Uniform(0, max(y), "y")
 
     result = bilby.run_sampler(
-        likelihood=likelihood, priors=priors, sampler='nestle', npoints=5000,
-        label=letter)
+        likelihood=likelihood,
+        priors=priors,
+        sampler="nestle",
+        npoints=5000,
+        label=letter,
+    )
     fig = result.plot_corner(quantiles=None)
